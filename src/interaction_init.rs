8da59ecba5cb8321429ebf6aea12d7d077a2e358
DoggySazHi <william.le.huy@gmail.com>
2021-12-22 20:11:19 -0800

    W.I.P. implement logging and interactions; still need to add command framework.

---
+++ src/interaction_init.rs
@@ -0,0 +1,25 @@
+use serenity::{
+    client::Context,
+    model::{
+        gateway::Ready,
+        interactions::application_command::ApplicationCommand
+    }
+};
+
+
+use log::{error, info};
+
+async fn register_slash_commands(ctx: Context, _: Ready) {
+    if let Err(ex) = ApplicationCommand::create_global_application_command(&ctx.http, |cmd| {
+        cmd.name("info").description("Get information about this bot.")
+    }).await {
+        error!("Cannot create slash command: {}", ex)
+    } else {
+        info!("Finished creating slash commands.")
+    }
+}
+
+pub async fn ready(ctx: Context, ready: Ready) {
+    info!("Logged in as {}", ready.user.name);
+    register_slash_commands(ctx, ready).await;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-pub mod models;
+mod models;
+mod interaction_init;
+mod command_framework;
 
 use std::env;
 use models::config::Config;
@@ -6,9 +8,14 @@ use std::fs;
 use serde_json;
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
+    client::{Client, Context, EventHandler},
+    model::{channel::Message, gateway::Ready, interactions::{
+        application_command::ApplicationCommandInteractionDataOptionValue,
+        Interaction,
+        InteractionResponseType,
+    }}
 };
+use log::{info, error};
 
 struct Handler;
 
@@ -17,27 +24,81 @@ impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!hello" {
             if let Err(why) = msg.channel_id.say(&ctx.http, "bruh").await {
-                println!("Error sending message: {:?}", why);
+                error!("Error sending message: {:?}", why);
             }
         }
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        interaction_init::ready(ctx, ready).await;
     }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "ping" => "Hey, I'm alive!".to_string(),
+                "id" => {
+                    let options = command
+                        .data
+                        .options
+                        .get(0)
+                        .expect("Expected user option")
+                        .resolved
+                        .as_ref()
+                        .expect("Expected user object");
+
+                    if let ApplicationCommandInteractionDataOptionValue::User(user, _member) =
+                    options
+                    {
+                        format!("{}'s id is {}", user.tag(), user.id)
+                    } else {
+                        "Please provide a valid user".to_string()
+                    }
+                },
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(ex) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                error!("Cannot respond to slash command: {}", ex);
+            }
+        }
+    }
+}
+
+async fn init_logger() -> std::io::Result<()> {
+    env_logger::init();
+    const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+    info!("Initializing cow v{}", VERSION.unwrap_or("<unknown>"));
+    info!("Reading from {}", env::current_dir()?.display());
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    print!("Reading from {}", env::current_dir()?.display());
+    if let Err(ex) = init_logger().await {
+        error!("Failed to initialize logger: {}", ex);
+    }
+
     let config_json = fs::read_to_string("config.json").expect("config.json not found");
     let config : Config = serde_json::from_str(&config_json).expect("config.json is malformed");
     let token = config.token;
 
-    let mut client = Client::builder(&token).event_handler(Handler).await.expect("Discord failed to initialize");
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .application_id(application_id)
+        .await
+        .expect("Discord failed to initialize");
 
     if let Err(ex) = client.start().await {
-        println!("Client error: {:?}", ex);
+        error!("Discord bot client error: {:?}", ex);
     }
 
     Ok(())
