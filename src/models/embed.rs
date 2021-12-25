7e4a1818d78cf5e37e1303d2507495c621b307cf
classAndrew <classAndrew@users.noreply.github.com>
2021-12-25 00:25:43 -0800

    add embed.rs, level up embed

--- src/models/embed.rs
+++ src/models/embed.rs
@@ -1,14 +1,13 @@
-use crate::models::User;
+use serenity::model::channel::Embed;
+use serenity::model::channel::Message;
 
-pub trait CowEmbed {
-    
+fn level_up(m: Message, prev_lvl: u64, next_lvl: u64) -> impl Fn(Message) {
+    // serenity wants this called like channel.send_message(ctx, level_up(...))
+    // Rust compiler broken so I can't test if this actually works. fixing atm
+    move |m| {
+        m.embed(|e| e
+            .title("Level up")
+            .description(format!("Leveled from {} to {}.", prev_lvl, next_lvl))
+        )
+    } 
 }
-
-pub struct LevelUp {
-    user: User,
-    prev_level: u64,
-    next_level: u64,
-}
-
-
-
